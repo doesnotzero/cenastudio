3b76a635bb79bb988bd3d2a0a8613f7bed32c0d5
elytraprod-hue <elytraprod@gmail.com>
2026-07-07 01:08:50 -0300

    fix: api/index.ts export from built dist

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,2 @@
 // Vercel serverless function entry point
-import { createApp } from "../server/app.js";
-
-const app = createApp();
-
-export default app;
+export { default } from "../dist/index.js";
