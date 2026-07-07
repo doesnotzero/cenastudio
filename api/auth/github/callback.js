396909d4e32da3ad3759cc30dea05533bfe9ef96
elytraprod-hue <elytraprod@gmail.com>
2026-07-07 03:16:02 -0300

    feat: add GitHub OAuth env vars and create auth endpoints

---
+++ api/auth/github/callback.js
@@ -0,0 +1,2 @@
+// Vercel serverless function for GitHub OAuth callback
+export { default } from '../../../dist/index.js';
---
+++ api/auth/login.js
@@ -0,0 +1,2 @@
+// Vercel serverless function for login
+export { default } from '../../dist/index.js';
